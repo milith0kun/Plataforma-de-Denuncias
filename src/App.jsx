84332be19ff11a4b0e97c4f03cc9ff486d4cbada
XDES <140984@unsaac.edu.pe>
2025-12-30 06:12:28 -0500

    fix: Eliminar ToastProvider duplicado de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { AuthProvider } from './contexts/AuthContext';
-import { ToastProvider } from './components/common/ToastContainer/ToastContainer';
 import AppRoutes from './routes/AppRoutes';
 import './App.css';
 
 function App() {
   return (
     <AuthProvider>
-      <ToastProvider>
-        <AppRoutes />
-      </ToastProvider>
+      <AppRoutes />
     </AuthProvider>
   );
 }
